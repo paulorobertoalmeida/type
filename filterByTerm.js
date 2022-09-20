71e5a1a74890f2cbbd38d5de0a3bdaa0097857ff
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-09-20 13:16:07 +0200

    Handling the return function

--- filterByTerm.js
+++ filterByTerm.js
@@ -1,12 +1,19 @@
 "use strict";
-function filterByTerm(input, searchTerm) {
+function filterByTerm(input, searchTerm, lookupKey) {
+    if (lookupKey === void 0) { lookupKey = "url"; }
     if (!searchTerm)
         throw Error("searchTerm cannot be empty");
     if (!input.length)
         throw Error("input cannot be empty");
     var regex = new RegExp(searchTerm, "i");
-    return input.filter(function (arrayElement) {
-        return arrayElement.url.match(regex);
+    return input
+        .filter(function (arrayElement) {
+        return arrayElement[lookupKey].match(regex);
     });
 }
-filterByTerm([{ url: "string1" }, { url: "string2" }, { url: "string3" }], "java");
+var obj1 = { url: "string1" };
+var obj2 = { url: "string2" };
+var obj3 = { url: "string3" };
+var arrOfLinks = [obj1, obj2, obj3];
+filterByTerm(arrOfLinks, "string3");
+////////////////////////////////
--- filterByTerm.ts
+++ filterByTerm.ts
@@ -2,18 +2,30 @@ interface ILink {
   description?: string;
   id?: number;
   url: string;
+  [index: string]: any;
 }
 
-function filterByTerm(input: Array<ILink>, searchTerm: string) {
+function filterByTerm(
+  input: Array<ILink>,
+  searchTerm: string,
+  lookupKey: string = "url"
+): Array<ILink> {
   if (!searchTerm) throw Error("searchTerm cannot be empty");
   if (!input.length) throw Error("input cannot be empty");
   const regex = new RegExp(searchTerm, "i");
-  return input.filter(function(arrayElement) {
-    return arrayElement.url.match(regex);
-  });
+  return input
+    .filter(function(arrayElement) {
+      return arrayElement[lookupKey].match(regex);
+    });
 }
 
-filterByTerm(
-  [{ url: "string1" }, { url: "string2" }, { url: "string3" }],
-  "java"
-);
+const obj1: ILink = { url: "string1" };
+const obj2: ILink = { url: "string2" };
+const obj3: ILink = { url: "string3" };
+
+const arrOfLinks: Array<ILink> = [obj1, obj2, obj3];
+
+filterByTerm(arrOfLinks, "string3");
+
+////////////////////////////////
+
