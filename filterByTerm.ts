2e166cd45620eef6fe89aaadb9609986983314b6
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-09-20 13:27:37 +0200

    last commit

--- filterByTerm.ts
+++ filterByTerm.ts
@@ -1,31 +1,25 @@
-interface ILink {
-  description?: string;
-  id?: number;
-  url: string;
-  [index: string]: any;
-}
+// the new label
+type ILinks = Array<ILink>;
+// the new label
 
 function filterByTerm(
-  input: Array<ILink>,
+  input: ILinks,
   searchTerm: string,
   lookupKey: string = "url"
-): Array<ILink> {
+): ILinks {
   if (!searchTerm) throw Error("searchTerm cannot be empty");
   if (!input.length) throw Error("input cannot be empty");
   const regex = new RegExp(searchTerm, "i");
-  return input
-    .filter(function(arrayElement) {
-      return arrayElement[lookupKey].match(regex);
-    });
+  return input.filter(function(arrayElement) {
+    return arrayElement[lookupKey].match(regex);
+  });
 }
 
 const obj1: ILink = { url: "string1" };
 const obj2: ILink = { url: "string2" };
 const obj3: ILink = { url: "string3" };
 
-const arrOfLinks: Array<ILink> = [obj1, obj2, obj3];
+const arrOfLinks: ILinks = [obj1, obj2, obj3];
 
 filterByTerm(arrOfLinks, "string3");
-
-////////////////////////////////
 
